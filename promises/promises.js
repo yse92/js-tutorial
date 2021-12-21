48b436acb1817dcb712497720842836796298ad5
Yaroslav <yarik49best@gmail.com>
2021-12-22 00:46:32 +0200

    promises 2

--- promises/promises.js
+++ promises/promises.js
@@ -11,9 +11,32 @@ function getPosts(){
         document.body.innerHTML  = output;
     },1000);
 }
-function createPost(post, callback){
-    setTimeout(()=>{
-        posts.push(post);
-        callback();
-    }, 2000);
+function createPost(post){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            posts.push(post);   
+            const error = true;    
+            if(!error){
+                resolve();//Успех
+            } 
+            else{
+                reject("Error: something went wrong");//Ошибка
+            }
+        }, 2000);
+    });
 }
+// createPost({title:'Post Three', body: 'This is post three'})
+// .then(getPosts)
+// .catch(err=>console.log(err));//getPosts - resolve, err=>console.log(err) - reject
+
+
+//Promise.all
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json());
+
+Promise.all([promise1, promise2, promise3, promise4])
+.then(values=>console.log(values));
