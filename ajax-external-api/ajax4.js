915c0a54569dc96c6bcdb7b39f0131cce28ec18e
Yaroslav <yarik49best@gmail.com>
2021-12-22 23:44:46 +0200

    ajax get form

--- ajax-external-api/ajax4.js
+++ ajax-external-api/ajax4.js
@@ -1,8 +1,13 @@
 document.getElementById('button').addEventListener('click', getName);
 
-function getName(){
+document.getElementById('getForm').addEventListener('submit', getName);
+function getName(e){
+    e.preventDefault();
+
+    var name = document.getElementById('name1').value;
+
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'process.php?name=Brad', true);
+    xhr.open('GET', 'process.php?name='+name, true);
     xhr.onload = function(){
         console.log(this.responseText);
     }
