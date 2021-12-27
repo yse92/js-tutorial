2169905b659a69ee24ba2cb560906edd84dbc64a
Yaroslav <yarik49best@gmail.com>
2021-12-27 14:03:22 +0200

    fetch api example

--- fetchapi/index.js
+++ fetchapi/index.js
@@ -20,10 +20,10 @@ function getUsers(){
         let output = '<h2>Users</h2>';
         data.forEach(function(user){
             output += `
-            <ul>
-                <li>ID: ${user.id}</li>
-                <li>Name: ${user.name}</li>
-                <li>Email: ${user.email}</li>
+            <ul class="list-group mb-3">
+                <li class="list-group-item">ID: ${user.id}</li>
+                <li class="list-group-item">Name: ${user.name}</li>
+                <li class="list-group-item">Email: ${user.email}</li>
             </ul>
             `;
         });
@@ -36,10 +36,10 @@ function getPosts(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => {
-        let output = '<h2>Posts</h2>';
+        let output = '<h2 class="mb-4">Posts</h2>';
         data.forEach(function(user){
             output += `
-                <div>
+                <div class="card-card-body mb-3">
                     <h3>${post.title}</h3>
                     <p>${post.body}</p>
                 </div>
